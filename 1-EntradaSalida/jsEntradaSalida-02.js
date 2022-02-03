8b9275952b91b61fcb35b4a3eabab8ec7acd80be
unknown <joaquincanepa99@gmail.com>
2022-02-03 16:08:31 -0300

    actualizacion entrada y salida ej 02 y 03

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,12 @@
-/*
+/*Joaquin Canepa
+Ejercicio 2
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	let nombre;
+	nombre=prompt("ingrese nombre");
+
+	alert("su nombre es "+nombre);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,11 +1,13 @@
-/*
+/*Joaquin Canepa
+Ejercicio 3
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
+	let nombreIngresado;
+
 	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	alert(nombreIngresado);
 
 }
 

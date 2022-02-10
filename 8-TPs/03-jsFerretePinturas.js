1c3bd65258dacedbde6527269485c69ee5021081
joaquincanepa <joaquincanepa99@gmail.com>
2022-02-10 18:27:54 -0300

    actualiazacion. if y tp hasta 03 entregados

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -1,4 +1,6 @@
-/*3.	Para el departamento de Pinturas:
+/*Joaquin Canepa
+	tp 03
+3.	Para el departamento de Pinturas:
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
@@ -12,7 +14,7 @@ function FahrenheitCentigrados ()
 
     temperaturaCentigrados=(temperaturaFahrenheit-32)*5/9;
 
-    alert(temperaturaCentigrados)
+    alert(temperaturaCentigrados);
 }
 
 function CentigradosFahrenheit () 
